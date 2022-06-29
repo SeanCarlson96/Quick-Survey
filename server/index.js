70a6abbfaf08be61fabf6a73fe584eb7ee4a9bf8
SeanCarlson96 <92127265+SeanCarlson96@users.noreply.github.com>
2022-06-29 17:42:16 -0500

    s

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4001;
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
