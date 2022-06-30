d2dd5097515952ef40becf8ec2374bfad3a75a4a
SeanCarlson96 <92127265+SeanCarlson96@users.noreply.github.com>
2022-06-30 12:46:05 -0500

    s

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const path = require('path');
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://QSuser:QSpass@quicksurveycluster.o321mlo.mongodb.net/qsDB", {useNewUrlParser: true}, {useUnifiedTopology: true});
+
+const surveyResponseSchema = {
+  name: String,
+  email: String,
+  age: Number,
+  hours: String,
+  adInfluence: String,
+  channels: String,
+  comments: String
+}
+
+const SurveyResponse = mongoose.model("SurveyResponse", surveyResponseSchema);
+
+
 
 const PORT = process.env.PORT || 4001;
 
@@ -12,6 +32,23 @@ app.get("/api", (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
+
+app.post("/", function(req, res) {
+  let newSurveyResponse = new SurveyResponse({
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age,
+    hours: req.body.hours,
+    adInfluence: req.body.adInfluence,
+    channels: req.body.channels,
+    comments: req.body.comments
+  });
+  newSurveyResponse.save();
+  res.redirect('/');
+})
+
+
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
   });
