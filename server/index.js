b83737d1b7330b796d939e0526f52998ff85e5f6
SeanCarlson96 <92127265+SeanCarlson96@users.noreply.github.com>
2022-06-30 13:45:59 -0500

    s

--- server/index.js
+++ server/index.js
@@ -20,6 +20,8 @@ const surveyResponseSchema = {
 
 const SurveyResponse = mongoose.model("SurveyResponse", surveyResponseSchema);
 
+
+
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
